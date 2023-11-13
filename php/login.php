0d8b6673f180d53611b1902038925ad2cffec46d
CassandraCHZ <19151765@aguascalientes.tecnm.mx>
2023-11-13 16:01:19 -0600

    tabla de usuarios lista y comienzo con login

---
+++ php/login.php
@@ -0,0 +1,50 @@
+<?php
+// Conexión a la base de datos (modifica las credenciales según tu configuración)
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "facebook";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Recuperar datos del formulario
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$phoneNumber = $_POST['phoneNumber'];
+$address = $_POST['address'];
+$gender = $_POST['gender'];
+$country = $_POST['country'];
+$religion = $_POST['religion'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Validar la longitud de la contraseña
+if (strlen($password) < 8) {
+    die("La contraseña debe tener al menos 8 caracteres.");
+}
+
+// Verificar si el correo electrónico ya está en uso
+$checkEmailQuery = "SELECT * FROM usuarios WHERE correo = '$email'";
+$result = $conn->query($checkEmailQuery);
+
+if ($result->num_rows > 0) {
+    // El correo electrónico ya está en uso
+    echo "email repetido";
+} else {
+    // Insertar datos en la base de datos
+    $sql = "INSERT INTO usuarios (primer_nombre, apellido_paterno, numero_celular, direccion, sexo, pais, religion, contraseña, correo)
+            VALUES ('$firstName', '$lastName', '$phoneNumber', '$address', '$gender', '$country', '$religion', '$password', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "success";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
---
+++ tableUsers.php
@@ -0,0 +1,63 @@
+<?php
+$con = mysqli_connect('localhost', 'root', '', 'facebook');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+</head>
+
+<body>
+    <table border="1" style="text-align:center; margin:auto;">
+        <h1 style="text-align:center; margin:auto;">Usuarios</h1>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>primer_nombre</th>
+                <th>apellido_paterno</th>
+                <th>numero_celular</th>
+                <th>direccion</th>
+                <th>sexo</th>
+                <th>pais</th>
+                <th>religion</th>
+                <th>contraseña</th>
+                <th>correo</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            <?php
+            $sql = "SELECT * FROM usuarios";
+            $result = mysqli_query($con, $sql);
+
+            while ($row = mysqli_fetch_array($result)) {
+            ?>
+
+                <tr>
+                    <td><?php echo $row['id'] ?></td>
+                    <td><?php echo $row['primer_nombre'] ?></td>
+                    <td><?php echo $row['apellido_paterno'] ?></td>
+                    <td><?php echo $row['numero_celular'] ?></td>
+                    <td><?php echo $row['direccion'] ?></td>
+                    <td><?php echo $row['sexo'] ?></td>
+                    <td><?php echo $row['pais'] ?></td>
+                    <td><?php echo $row['religion'] ?></td>
+                    <td><?php echo $row['contraseña'] ?></td>
+                    <td><?php echo $row['correo'] ?></td>
+                </tr>
+
+            <?php
+            }
+            $con->close();
+            ?>
+
+        </tbody>
+    </table>
+</body>
+
+</html>
